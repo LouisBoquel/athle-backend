6f12d9c570941b77456f4abafec871d72a1ae917
LouisBoquel <73896483+LouisBoquel@users.noreply.github.com>
2025-07-18 11:42:02 +0200

    Delete cron_scraper.php

--- cron_scraper.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Exécuter le script Python (nécessite que le serveur autorise exec)
-// À adapter selon l'installation Python sur Infomaniak !
-exec("python3 /home/clients/88825ac5e5f79e7ac027b682eb1b2848/sites/thegreenpadel.fr/scraper/scraper_full_year.py > /home/clients/88825ac5e5f79e7ac027b682eb1b2848/sites/thegreenpadel.fr/logs/cron.log 2>&1 &");
-echo "OK";
-?>
